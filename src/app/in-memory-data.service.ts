78da8ae3a029b5126a721396f1cd93fbe1f76418
Anand Simmy <anandsimmy7@gmail.com>
2018-06-15 23:59:16 +0530

    notes added

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -3,16 +3,11 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const heroes = [
-      { id: 11, name: 'Mr. Nice' },
-      { id: 12, name: 'Narco' },
-      { id: 13, name: 'Bombasto' },
-      { id: 14, name: 'Celeritas' },
-      { id: 15, name: 'Magneta' },
-      { id: 16, name: 'RubberMan' },
-      { id: 17, name: 'Dynama' },
-      { id: 18, name: 'Dr IQ' },
-      { id: 19, name: 'Magma' },
-      { id: 20, name: 'Tornado' }
+      { id: 1, name: 'Watch Highlights' },
+      { id: 2, name: 'Get Haircut' },
+      { id: 3, name: 'Download Movie' },
+      { id: 4, name: 'Redeem Coupon' },
+      { id: 5, name: 'Bike Servicing' }
     ];
     return {heroes};
   }
