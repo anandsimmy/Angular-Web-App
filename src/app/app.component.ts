4e8906f41510c9628dabcdea8c57206380fb9de3
Anand Simmy <anandsimmy7@gmail.com>
2018-06-16 00:49:31 +0530

    meta module imported

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  
 })
 export class AppComponent {
+  constructor(private meta: Meta) {
+    this.meta.addTag({ name: 'Description', content: 'Notes - My Angular App' });
+  }
   title = 'Notes';
 }
